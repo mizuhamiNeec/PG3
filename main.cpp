5c18beec01b95ae9025f7a0f7c20cf754b52207d
mizuhamiNeec <146906652+mizuhamiNeec@users.noreply.github.com>
2024-09-30 11:30:16 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <cstdio>
 
+template <typename T>
+T Min(T a, T b) {
+	if (a < b) { return a; }
+	if (b < a) { return b; }
+	return 0;
+}
+
+template <>
+char Min<char>(char a, char b) {
+	printf("数字以外は代入できません。");
+	return 0;
+}
+
 int main() {
 
-	printf("Hello World !!!!");
-
-	printf("こんにちは!!世界!");
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(114.0f, 514.0f));
+	printf("%lf\n", Min<double>(114.0, 514.0));
+	printf("%c\n", Min<char>('a', 'b'));
 
 	return 0;
 }
